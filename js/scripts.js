b4174c0f1c42254a0329ce9a08dc7703c602fa7f
Bryan Jones <thebeard@me.com>
2018-02-12 16:17:48 -0800

    Simple Apple style jQuery slider

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    var totalWidth = 0;
+    var positions = new Array();
+
+    $('#slides .slide').each(function(i){
+        
+        //Get slider widths
+        positions[i] = totalWidth;
+        totalWidth += $(this).width();
+
+        //Check Widths
+        if(!$(this).width()) {
+            alert("Please add a width to your images");
+            return false; 
+        }
+    })
+
+
+    // Set Width
+    $('#slides').width(totalWidth);
+
+    //Menu item click handler
+    $('#menu ul li a').click(function(e, keepScroll){
+        // Remove active class and add inactive
+        $('li.product').removeClass('active').addClass('inactive');
+        // Add Active class to Parent
+        $(this).parent().addClass('active');
+
+        var pos = $(this).parent().prevAll('.product').length;
+
+        $('#slides').stop().animate({marginLeft:-positions[pos]+'px'}, 450);
+
+        // Prevent default
+        e.preventDefault();
+
+
+        if(!autoScroll) clearInterval(itvl);
+   
+    })
+
+
+    // Make first image active
+    $('#menu ul li.product:first').addClass('active').siblings().addClass('inactive');
+
+    // Autoscroll
+    var current =1;
+    function autoScroll(){
+        if(current === -1) return false;
+
+        $('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click',[true]);
+        current++;
+    }
+
+
+    // Duration for auto-scroll
+    var duration = 5;
+    var itvl = setInterval(function(){autoScroll()}, duration*1000);
+
+
+});
